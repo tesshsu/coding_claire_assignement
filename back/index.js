b3fe77b1bd07949512d09d6b28ffc7bdd0c35fd6
yating <yating@itekcom.com>
2024-04-04 12:33:07 +0200

    Test vercel json

--- back/index.js
+++ back/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3001;
-//const port = process.env.PORT || 3000;
+//const port = process.env.PORT || 3001;
 const path = require('path');
-
+const port = process.env.PORT || 3000;
 const states = require('./states');
 
 app.use(express.json());
@@ -40,7 +39,8 @@ app.post('/emails', (req, res) => {
 
 
   // Serve any static files
-app.use(express.static(path.join(__dirname, '../front')));
+//app.use(express.static(path.join(__dirname, '../front')));
+app.use(express.static(path.join(__dirname, '..', 'front')));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
